13876f81f5713191f94e102e752ce2925b2dd1c2
Alex <morozspb87@gmail.com>
2023-11-16 03:12:42 +0300

    new funcrion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+    }
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -7,11 +15,54 @@ const personalMovieDB = {
     privat:false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?', ''), 
-d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+function rememberMytFilms(){
+    for (let i= 0; i < 2; i++)
+    {
+        const a = prompt('Один из последних просмотренных фильмов?', ''), 
+        b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b !=null && a != '' && a != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    }
+    else { 
+        console.log('EROOR');
+        i--;
+    }
+        
+    }
+}
+rememberMytFilms();
+
+
+
+function detectPersnalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log(malo);
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log(GOOOD);
+    } else if (personalMovieDB.count >= 30) {
+        console.log(NICE);
+    } else {
+        console.log(ERROR);
+    }
+}
+etectPersnalLevel();
+
+
+function showMyDB(hidden) {
+if (!hidden) {
+    console.log(personalMovieDB);
+}
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGeneres() {
+    for (let i = 1; i<=3; i++){
+    personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+}
+}
+writeYourGeneres();
